825da91bfe4634fb0931b2280f62a88e9ee0c7c3
Andrew Wittrock <andy@wittrock.us>
2019-03-31 17:50:57 -0500

    Enable menu clicks

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -9,19 +9,27 @@ import './App.css';
 
 const history = createBrowserHistory();
 
+const goTo = (url:string) => () => {
+  history.push(url);
+};
+
+const redirectTo = (url:string) => () => {
+  location.href = url;
+};
+
 export const App = () => 
   <DocumentTitle title={config.site.title}>
     <Router history={history}>
       <Layout style={{maxWidth: config.site.width, margin: "auto", position: "relative"}}>
         <Layout.Header style={{position: "fixed", top: 0, width: "100%", maxWidth: config.site.width, zIndex: 999}}>
           <Menu theme="dark" mode="horizontal">
-            <Menu.Item>andy.wittrock.us</Menu.Item>
-            <Menu.Item>Front End</Menu.Item>
-            <Menu.Item>3D Graphics</Menu.Item>
-            <Menu.Item>Algorithms</Menu.Item>
-            <Menu.Item>Procedural Content</Menu.Item>
-            <Menu.Item>Utilities</Menu.Item>
-            <Menu.Item style={{float: "right"}}>Résumé</Menu.Item>
+            <Menu.Item onClick={goTo('/')}>andy.wittrock.us</Menu.Item>
+            <Menu.Item onClick={goTo('/tags/Front End')}>Front End</Menu.Item>
+            <Menu.Item onClick={goTo('/tags/3D Graphics')}>3D Graphics</Menu.Item>
+            <Menu.Item onClick={goTo('/tags/Algorithms')}>Algorithms</Menu.Item>
+            <Menu.Item onClick={goTo('/tags/Procedural Content')}>Procedural Content</Menu.Item>
+            <Menu.Item onClick={goTo('/tags/Utilities')}>Utilities</Menu.Item>
+            <Menu.Item onClick={goTo('/resume')} style={{float: "right"}}>Résumé</Menu.Item>
           </Menu>
         </Layout.Header>
         <Layout.Content style={{marginTop: "46px", padding: "8px"}}>
@@ -31,7 +39,9 @@ export const App = () =>
         </Layout.Content>
         <Layout.Header style={{position: "fixed", bottom: 0, width: "100%", maxWidth: config.site.width}}>
           <Menu theme="dark" mode="horizontal">
-            <Menu.Item style={{float: "right"}}>&lt;source&gt;</Menu.Item>
+            <Menu.Item onClick={redirectTo('https://github.com/DaemonAlchemist/andy.wittrock.us')} style={{float: "right"}}>
+              &lt;source&gt;
+            </Menu.Item>
           </Menu>
         </Layout.Header>
       </Layout>
