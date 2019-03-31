5eb42b9f572f0c4d595a0edf7fa53aa5d9c4445a
Andrew Wittrock <andy@wittrock.us>
2019-03-31 16:18:57 -0500

    Cleanup page props

--- src/components/Pages/TestPage.component.tsx
+++ src/components/Pages/TestPage.component.tsx
@@ -3,8 +3,15 @@ import { DropCap } from "../DropCap";
 import { Image } from "../Image";
 import { Page } from "../Page";
 
+const pageProps:IPageProps = {
+    published: "2019-01-03",
+    tags: ["Front End", "Algorithms"],
+    title: "Test Page",
+    updated: "2019-01-23",
+}
+
 export const TestPageComponent = () =>
-    <Page title="Test Page" published={"2019-01-03"} updated={"2019-01-23"} tags={["Front End", "Algorithms"]}>
+    <Page {...pageProps}>
         <p><DropCap>L</DropCap>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <Image left src="https://assets.fireside.fm/file/fireside-images/podcasts/images/b/bc7f1faf-8aad-4135-bb12-83a8af679756/header.jpg" alt="Test Image">
