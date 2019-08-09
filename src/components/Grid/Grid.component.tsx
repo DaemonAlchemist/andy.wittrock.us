fe0aaa45b82e88682708a19e7b75c533228584e9
Andrew Wittrock <andy@wittrock.us>
2019-08-09 15:43:42 -0500

    Fix grid column sizing

--- src/components/Grid/Grid.component.tsx
+++ src/components/Grid/Grid.component.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
-const generateColumns = (count:number) => new Array(count).fill(`${100 / count}%`).join(" ");
+const generateColumns = (count:number) => new Array(count)
+    .fill(`calc((100% - ${count - 1}*8px) / ${count})`)
+    .join(" ");
 
 export const Grid = (props:IGridProps) =>
     <div className="grid" style={{
