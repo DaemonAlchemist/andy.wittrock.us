18dfb71bb8d8698687f5eec675ed4adc757e8bf6
Andrew Wittrock <andy@wittrock.us>
2019-08-19 10:40:43 -0500

    Refactor image styles

--- src/components/Image/Image.component.tsx
+++ src/components/Image/Image.component.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
 import "./Image.less";
 
-const imageContainerStyle = (props:IImageProps):React.CSSProperties => ({
-    border: "solid 1px",
-    boxSizing: "border-box",
-    float: props.left ? "left" : props.right ? "right" : undefined,
-    marginBottom: "8px",
-    marginLeft: props.right ? "1em" : 0,
-    marginRight: props.left ? "1em" : 0,
-    padding: "8px",
-    textAlign: "center",
-    width: `${props.left || props.right
-        ? (props.small ? "25%" : "50%")
-        : (props.small ? "50%" : "100%")
-    }`,
-});
-
 export const Image = (props:IImageProps) =>
     <div
-        className="image"
-        style={imageContainerStyle(props)}
+        className={
+            ['image']
+            .concat(props.left ? ["left"] : [])
+            .concat(props.right ? ["right"] : [])
+            .concat(props.small ? ["small"] : [])
+            .join(" ")
+        }
     >
         <img
             src={props.src}
